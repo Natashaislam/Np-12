89810029849987cd6d7f1407cb19da47b3de6b8e
Natashaislam <mimmantasha555@gmail.com>
2024-12-23 14:50:53 +0600

    first commit

---
+++ restuaran.js
@@ -0,0 +1,6 @@
+let main-button = document .getElementById("main-button")
+let main manu = document .getElementById("main manu");
+main-button .addEventListener("click", ()=> {
+main manu.classList.toggle("hidden");
+
+})
